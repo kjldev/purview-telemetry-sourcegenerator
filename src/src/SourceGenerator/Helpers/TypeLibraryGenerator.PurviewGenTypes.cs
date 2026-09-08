9ab479e6638f0e51b2464da4aaac131cb43942e1
Kieron Lanning <kieronlanning@users.noreply.github.com>
2026-09-08 08:01:03 +0100

    feat: add Purview telemetry attributes to TypeLibraryGenerator

---
+++ src/src/SourceGenerator/Helpers/TypeLibraryGenerator.PurviewGenTypes.cs
@@ -0,0 +1,119 @@
+namespace Purview.Telemetry.SourceGenerator.Helpers;
+
+partial class TypeLibraryGenerator
+{
+	// Activities
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity ActivitySourceGenerationAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity ActivitySourceAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity ActivityAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity EventAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity ContextAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity BaggageAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity EscapeAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity StatusDescriptionAttribute = default;
+
+	// Logging
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity LoggerGenerationAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity LoggerAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity LogAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity LogPrefixType = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity LoggerGenerationMode = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity ExpandEnumerableAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity TraceAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity DebugAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity InfoAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity WarningAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity ErrorAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity CriticalAttribute = default;
+
+	// Metric
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity MeterGenerationAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity MeterAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity MeterNameGenerationType = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity InstrumentMeasurementAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity AutoCounterAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity CounterAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity UpDownCounterAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity HistogramAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity ObservableCounterAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity ObservableUpDownCounterAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity ObservableGaugeAttribute = default;
+
+	// Shared
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity TagAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity ExcludeAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity TelemetryGenerationAttribute = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity Targets = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity NamingConvention = default;
+
+	[TypeRef(PurviewTelemetryNamespace, includeInGetTypes: true)]
+	static readonly TypeIdentity ExcludeTargetsAttribute = default;
+}
